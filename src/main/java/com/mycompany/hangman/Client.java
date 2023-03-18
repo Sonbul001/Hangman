2110de8d0c2bc5aaaaec5e4aee3b4a6dadbd41ac
Karim Sonbul <sonbul.karim@gmail.com>
2023-03-18 18:44:16 +0200

    implemented files and config and finished menu

--- src/main/java/com/mycompany/hangman/Client.java
+++ src/main/java/com/mycompany/hangman/Client.java
@@ -2,8 +2,6 @@ package com.mycompany.hangman;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
@@ -11,18 +9,56 @@ import java.util.Scanner;
 
 public class Client {
 
+    
     public static void main(String[] args) {
         String host = "localhost";
         int port = 6766;
         try {
             Socket socket = new Socket(host, port);
+            System.out.println("Connection Successful");
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String out, in;
             Scanner input = new Scanner(System.in);
 
+            // reads the menu
             in = reader.readLine();
             System.out.println(in);
+            in = reader.readLine();
+            System.out.println(in);
+            in = reader.readLine();
+            System.out.println(in);
+            in = reader.readLine();
+            System.out.println(in);
+
+            // types in response 1 for login 2 for register
+            while(true){
+                out = input.nextLine();
+                if(out.equalsIgnoreCase("1") || out.equalsIgnoreCase("2"))
+                    break;
+                else
+                    System.out.println("Wrong input");
+            }
+            writer.write(out);
+            writer.newLine();
+            writer.flush();
+
+            // reads response for either login or register
+            in = reader.readLine();
+            System.out.println(in);
+            out = input.nextLine();
+            writer.write(out);
+            writer.newLine();
+            writer.flush();
+
+            // types in credentials
+            in = reader.readLine();
+            System.out.println(in);
+            if(in.equalsIgnoreCase("401 UNAUTHORIZED") || in.equalsIgnoreCase("404 NOT FOUND") || in.equalsIgnoreCase("Username already exists")){
+                System.exit(0);
+            }
+            
+            // reads the menu
             in = reader.readLine();
             System.out.println(in);
             in = reader.readLine();
@@ -30,20 +66,18 @@ public class Client {
             in = reader.readLine();
             System.out.println(in);
             
-            out = input.nextLine();
+            // choose singleplayer or multiplayer
+            while(true){
+                out = input.nextLine();
+                if(out.equalsIgnoreCase("1") || out.equalsIgnoreCase("2"))
+                    break;
+                else
+                    System.out.println("Wrong input");
+            }
             writer.write(out);
             writer.newLine();
-            writer.flush();
-
-            in = reader.readLine();
-            System.out.println(in);
-            out = input.nextLine();
-            writer.write(out);
-            writer.newLine();
-            writer.flush();
-
-            in = reader.readLine();
-            System.out.println(in);
+            writer.flush();            
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/mycompany/hangman/Server.java
+++ src/main/java/com/mycompany/hangman/Server.java
@@ -14,11 +14,37 @@ import java.util.Scanner;
 public class Server implements Runnable {
 
     protected Socket socket;
+    protected static Thread t;
+    protected static File creds;
+    protected static File history;
+    protected static File config;
+    protected static File lookup;
+    protected static int incorrectGuesses;
+    protected static int minPlayers;
+    protected static int maxPlayers;
+
+    public static void loadFiles() {
+        creds = new File("credentials.txt");
+        history = new File("history.txt");
+        config = new File("config.txt");
+        lookup = new File("lookup.txt");
+        try {
+            Scanner reader = new Scanner(config);
+            String[] data = reader.nextLine().split("-");
+            incorrectGuesses = Integer.parseInt(data[1]);
+            data = reader.nextLine().split("-");
+            minPlayers = Integer.parseInt(data[1]);
+            data = reader.nextLine().split("-");
+            maxPlayers = Integer.parseInt(data[1]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static String register(String username, String password, String name) {
         boolean exists = false;
         try {
-            File creds = new File("credentials.txt");
             Scanner reader = new Scanner(creds);
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
@@ -30,7 +56,7 @@ public class Server implements Runnable {
             }
             reader.close();
             if (!exists) {
-                BufferedWriter writer = new BufferedWriter(new FileWriter("credentials.txt",true));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("credentials.txt", true));
                 writer.write(username + " " + password + " " + name);
                 writer.newLine();
                 writer.close();
@@ -45,20 +71,23 @@ public class Server implements Runnable {
 
     public static String login(String username, String password) {
         try {
-            File creds = new File("credentials.txt");
             Scanner reader = new Scanner(creds);
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
                 String[] parsed = data.split(" ");
-                if (parsed[0].equals(username) && parsed[1].equals(password)) {
-                    return "Welcome " + parsed[2];
+                if (parsed[0].equals(username)) {
+                    if (parsed[1].equals(password)) {
+                        return "Welcome " + parsed[2];
+                    } else {
+                        return "401 UNAUTHORIZED";
+                    }
                 }
             }
             reader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return "Incorrect Username Or Password";
+        return "404 NOT FOUND";
     }
 
     @Override
@@ -67,27 +96,27 @@ public class Server implements Runnable {
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String out, in;
-            
+
             out = "Welcome to hangman";
             writer.write(out);
             writer.newLine();
             writer.flush();
-            
+
             out = "Please choose one of the following:";
             writer.write(out);
             writer.newLine();
             writer.flush();
-            
+
             out = "1. Login";
             writer.write(out);
             writer.newLine();
             writer.flush();
-            
+
             out = "2. Register";
             writer.write(out);
             writer.newLine();
             writer.flush();
-            
+
             in = reader.readLine();
             if (in.equalsIgnoreCase("1")) {
                 out = "Please enter your username and password separated by a space";
@@ -95,6 +124,7 @@ public class Server implements Runnable {
                 writer.newLine();
                 writer.flush();
 
+                // input format should be (username password)
                 in = reader.readLine();
                 String[] info = in.split(" ");
                 System.out.println(info[0]);
@@ -103,13 +133,13 @@ public class Server implements Runnable {
                 writer.write(result);
                 writer.newLine();
                 writer.flush();
-            }
-            else if (in.equalsIgnoreCase("2")) {
+            } else if (in.equalsIgnoreCase("2")) {
                 out = "Please enter your username and password and name separated by a space";
                 writer.write(out);
                 writer.newLine();
                 writer.flush();
-                
+
+                // input format should be (username password name)
                 in = reader.readLine();
                 String[] info = in.split(" ");
                 System.out.println(info[0]);
@@ -118,6 +148,29 @@ public class Server implements Runnable {
                 writer.write(result);
                 writer.newLine();
                 writer.flush();
+            }else{
+                
+            }
+            out = "Please choose one of the following:";
+            writer.write(out);
+            writer.newLine();
+            writer.flush();
+
+            out = "1. Singleplayer";
+            writer.write(out);
+            writer.newLine();
+            writer.flush();
+
+            out = "2. Multiplayer";
+            writer.write(out);
+            writer.newLine();
+            writer.flush();
+            
+            in = reader.readLine();
+            if(in.equalsIgnoreCase("1")){
+                // do singleplayer code
+            }else if(in.equalsIgnoreCase("2")){
+                // do multiplayer code
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -130,14 +183,15 @@ public class Server implements Runnable {
 
     public static void main(String[] args) throws Exception {
         int port = 6766;
-        System.out.println("Math Server is running...");
+        System.out.println("Server is running...");
         ServerSocket serverSocket = new ServerSocket(port);
+        loadFiles();
         while (true) {
             try {
                 Socket socket = serverSocket.accept();
                 Server server = new Server();
                 server.setSocket(socket);
-                Thread t = new Thread(server);
+                t = new Thread(server);
                 t.start();
 
             } catch (Exception e) {
